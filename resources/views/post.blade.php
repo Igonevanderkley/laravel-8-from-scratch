24faea7967a31bea03d53a7589f64709ed28db28
Igone van_der_Kley <igone@vdkley.nl>
2024-03-13 16:37:29 +0100

    Post now displays writer

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,12 @@
 <x-layout>
     <article>
+
+        <p>
+            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
+
         <h2>{{ $post->title }}</h2>
+
         <div>
             {!! $post->body !!}
         </div>
