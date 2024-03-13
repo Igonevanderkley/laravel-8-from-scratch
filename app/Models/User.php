712d52107f44f6f3b300db8e80e138c1c373aceb
Igone van_der_Kley <igone@vdkley.nl>
2024-03-13 16:33:26 +0100

    Relation post has many users added

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts() { //you have a user and want to grab theyre posts
+        return $this->hasMany(Post::class);
+    }
 }
