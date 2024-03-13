9c01aacc0f0f6cead423d079672257b97e802b44
Igone van_der_Kley <igone@vdkley.nl>
2024-03-13 16:32:49 +0100

    Relationship post belongs to user added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function category()
+    {
+        //hadOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
