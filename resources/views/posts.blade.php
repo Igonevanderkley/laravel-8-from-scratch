722bc9c593c598e4f2ab82204ef9d6e3dfd3e94d
Igone van_der_Kley <igone@vdkley.nl>
2024-03-13 16:38:40 +0100

    view now displays category link

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,19 @@
 <x-layout>
-        @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{ $post->id }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
+    @foreach ($posts as $post)
+        <article>
+            <p>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
+
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
 </x-layout>
