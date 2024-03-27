091f68774cf462bc57df43d7ce53983965ba4c82
Igone van_der_Kley <igone@vdkley.nl>
2024-03-27 10:18:16 +0100

    views updated

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
     <article>
 
         <p>
-            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="author/{{ $post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
         <h2>{{ $post->title }}</h2>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,8 @@
     @foreach ($posts as $post)
         <article>
             <p>
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                By <a href="author/{{ $post->author->username}}">{{ $post->author->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
 
             <h1>
